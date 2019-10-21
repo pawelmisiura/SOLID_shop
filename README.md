d451e8390b85fd03baa39e3717981a3ff33d23ad
Pawel Misiura <pawelmisiura1@gmail.com>
2019-10-21 15:24:44 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+#SOLID 
+
+S- single repository
+O- open/closed repository
+L- liskov Substitution
+I- Interface segregation
+D- Dependency Inversion
---
+++ src/main/java/com/spartaglobal/SOLID_shop/App.java
@@ -0,0 +1,13 @@
+package com.spartaglobal.SOLID_shop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
